9b05fad2427ca84b77f06f26c31bb2f91528732f
Vadim Shegolev <keepman12121998@gmail.com>
2020-10-05 18:17:42 +0600

    Add project

---
+++ Shegolev/task1/AlphabeticalText/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AlphabeticalText
+{
+    class CheckAlphabetical
+    {
+        public static int Sort(string text)
+        {
+            text = text.Replace(" ", "").Replace(",", "").Replace(".", "");
+            for (int i = 1; i < text.Length; i++)
+            {
+                if(text[i - 1] > text[i])
+                {
+                    return i+1;
+                }
+            }
+            Console.WriteLine("Текст в алфавитном порядке");
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст: ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine(Sort(text));
+        }
+    }
+}
