3a910fdc7d6d07b4b4f88750dca207c0b16338f0
VadimShegolev <keepman12121998@gmail.com>
2020-10-06 23:25:18 +0600

    New project

---
+++ Shegolev/tasknew/tasknew/AlphabetText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasknew
+{
+    public class AlphabetText
+    {
+        private string mTxt;
+        public AlphabetText(string txt)
+        {
+            mTxt = txt;
+        }
+
+        public int Sort()
+        {
+            var result = (from c in mTxt where Char.IsLetter(c) select c).ToArray();
+            for (int i = 1; i < mTxt.Length; i++)
+            {
+                if (mTxt[i - 1] > mTxt[i])
+                {
+                    return i + 1;
+                }
+            }
+            Console.WriteLine("Текст в алфавитном порядке");
+            return 0;
+        }
+    }
+}
---
+++ Shegolev/tasknew/tasknew/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tasknew
+{
+    internal class Program : GeneratedText
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа для вывода позиции символа, который нарушает алфавитный порядок");
+            Console.WriteLine("Enter");
+
+            new GeneratedText();
+            
+        }
+    }
+}
