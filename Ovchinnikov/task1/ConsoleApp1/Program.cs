f281e22ac337ad34f6ddfe13f6b3b904921fe82e
aovchinnikov <aovchinnikov>
2020-10-08 16:34:44 +0600

    add

--- Ovchinnikov/task1/ConsoleApp1/Program.cs
+++ Ovchinnikov/task1/ConsoleApp1/Program.cs
@@ -12,22 +12,54 @@ namespace main
             sender msg = new sender();
             while (true)
             {
-                Console.WriteLine("Введите слова через запятую на латинском алфавите с точкой в конце");
-                string str = Console.ReadLine();
-
-                if (str.IndexOf('.') == -1)
+                msg.sendHello();
+                int answ = Console.ReadKey().KeyChar;
+                switch(answ)
                 {
-                    msg.noFoundedDot();
-                }
-                else
-                {
-
-                    str = str.Substring(0, str.IndexOf('.'));
-                    string[] words = str.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                    if (check.checkLong(words) && check.checkMuch(words) && check.checkAbc(str))
+                    case '1':
+                    Console.Clear();
+                    msg.sendAbtInput();
+                    string str = Console.ReadLine();
+                    if (str.IndexOf('.') == -1)
                     {
-                        msg.sendResult(words);
+                        msg.noFoundedDot();
                     }
+                    else
+                    {
+
+                        str = str.Substring(0, str.IndexOf('.'));
+                        string[] words = str.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (check.checkLong(words) && check.checkMuch(words) && check.checkAbc(str))
+                        {
+                            msg.sendResult(words);
+                        }
+                    }
+                    break;
+
+                    case '2':
+                    Console.Clear();
+                    string str1 = "first,word,last,life.ЫЫ";
+                    if (str1.IndexOf('.') == -1)
+                    {
+                        msg.noFoundedDot();
+                    }
+                    else
+                    {
+
+                        str1 = str1.Substring(0, str1.IndexOf('.'));
+                        string[] words = str1.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (check.checkLong(words) && check.checkMuch(words) && check.checkAbc(str1))
+                        {
+                            msg.sendResult(words);
+                        }
+                    }
+                    break;
+                    case '3':
+                    Console.Clear();
+                    //generator generator = new generator();
+                    //char[] Alphabet =
+                    //generator.generate();
+                    break;
                 }
             }
         }
