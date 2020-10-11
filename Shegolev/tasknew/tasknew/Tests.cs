777acf39bf267abcca2b6336e89a59e6990c80af
VadimShegolev <keepman12121998@gmail.com>
2020-10-11 23:31:30 +0600

    Added Queue

--- Shegolev/tasknew/tasknew/Tests.cs
+++ Shegolev/tasknew/tasknew/Tests.cs
@@ -4,14 +4,16 @@ namespace tasknew
 {
     public class Tests
     {
-       String TestEmptyString(string s) {
+       String TestEmptyString(string s)
+       {
            if (String.IsNullOrEmpty(s))
                 return "Пустая строка";
             else
                 return String.Format("(\"{0}\") is neither null nor empty", s);
        }
 
-        public static void TestCase1() {
+        public static void TestCase1()
+        {
             string str1 = "АБВГДЕЕЖ";
             int i = 0;
             Console.WriteLine("Исходная строка: " + str1);
@@ -19,12 +21,13 @@ namespace tasknew
             i = teststs.Sort(str1);
             if (i == 0)
             {
-                Console.WriteLine("В алфавитном порядке");
+                Console.WriteLine("Текст в алфавитном порядке");
             }
             else Console.WriteLine("{0} - первый символ, который нарушает порядок", i);
         }
 
-        public static void TestCase2() {
+        public static void TestCase2()
+        {
             string str1 = "АбаГДеЕЖ";
             int i = 0;
             Console.WriteLine("Исходная строка: " + str1);
@@ -32,7 +35,7 @@ namespace tasknew
             i = teststs.Sort(str1);
             if (i == 0)
             {
-                Console.WriteLine("В алфавитном порядке");
+                Console.WriteLine("Текст в алфавитном порядке");
             }
             else Console.WriteLine("{0} - первый символ, который нарушает порядок", i);
         }
