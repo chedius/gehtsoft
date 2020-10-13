ae8cb50716f2007017d92a9f809af24d1b0ff918
Pozharov <chedius232@gmail.com>
2020-10-13 23:27:39 +0300

    Menu translated into English, Classes split into separate files

---
+++ Pozharov/Task1/ClassTable/TextPrinter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassTable
+{
+    //Вывод текстового меню
+    class TextPrinter
+    {
+        public static void MainMenu()
+        {
+            Console.WriteLine("Choose one of the following actions: \n1.Hexadecimal multiplication table output \n2.Random array output \n3.Exit");
+        }
+
+        public static void Dimensity()
+        {
+            Console.WriteLine("Please enter dimensity of the array:");
+        }
+
+        public static void Default()
+        {
+            Console.WriteLine("Please enter number of action");
+        }
+    }
+}
