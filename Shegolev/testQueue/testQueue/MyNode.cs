4daf3c7a57c282a0f3d3792b64f7d1ff060fd4e0
VadimShegolev <keepman12121998@gmail.com>
2020-10-13 22:29:54 +0600

    Fix Queue

---
+++ Shegolev/testQueue/testQueue/MyNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testQueue
+{
+    class MyNode
+    {
+        public int Value { get; set; }
+        public MyNode Next { get; set; }
+        public MyNode Prev { get; set; }
+    }
+}
