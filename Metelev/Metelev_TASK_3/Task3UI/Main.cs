b52951c9f8bc416b1355fbca82bce1ace44350e9
nedostudent <spicediller@mail.ru>
2020-10-15 15:46:44 +0600

    Added Task 3 without LINQ

---
+++ Metelev/Metelev_TASK_3/Task3UI/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task3UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseOrder rvr = new ReverseOrder();
+            Console.WriteLine("Enter the number of strings");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string s = rvr.Reverse(num);
+            Console.WriteLine("\nFirst characters of each line in reverse order: " + s);
+        }
+    }
+}
---
+++ Metelev/Metelev_TASK_3/run.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+cd Task3UI
+dotnet run
+pause
